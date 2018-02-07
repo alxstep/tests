f59add6c53b843ee0b55bc441632b13bfc1d11d0
Alex Stepenko <astepenko@aqueti.com>
2018-02-07 12:11:55 -0500

    tes

---
+++ _test.cpp
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+TEST(sample_test_case, sample_test)
+{
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char** argv) 
+{ 
+    testing::InitGoogleTest(&argc, argv); 
+    RUN_ALL_TESTS();
+    
+    system("pause");
+
+    return 0;
+}
